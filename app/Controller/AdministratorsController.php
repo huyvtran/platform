94363a5eae95a4885ad8b83483dc188e0a871e05
QuanVu <quanhongvu@gmail.com>
2017-05-20 10:19:25 +0700

    update log

--- app/Controller/AdministratorsController.php
+++ app/Controller/AdministratorsController.php
@@ -48,7 +48,7 @@ class AdministratorsController extends AppController{
 		if ($name) {
 			$lines = $this->request->query('lines');
 			if (empty($lines))
-				$lines = 100;
+				$lines = 500;
 
 			ob_start();
 			passthru("tail -n $lines " . TMP . 'logs' . DS . $name, $result);
