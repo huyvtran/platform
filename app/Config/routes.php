7bd245838cc357820e20e030ef71dc52999e620d
QuanVu <quanhongvu@gmail.com>
2017-05-22 00:43:46 +0700

    update routes

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -24,16 +24,7 @@ Router::connect('/news/:category/:slug',
 # this routing support SDK only ?
 Router::connect('/news/*', array('controller' => 'categories', 'action' => 'index'));
 
-Router::connect('/tag/:tag', array('controller' => 'categories', 'action' => 'index'));
 Router::connect('/admin', array('controller' => 'administrators', 'action' => 'index', 'admin' => true));
-Router::connect('/payment/*', array('controller' => 'webp', 'action' => 'index'));
-Router::connect('/policy', array('controller' => 'articles', 'action' => 'view','slug'=>'policy','category'=>'legal'));
-Router::connect('/terms', array('controller' => 'articles', 'action' => 'view','slug'=>'terms','category'=>'legal'));
-
-Router::connect('/support/:gameAlias/:slug/:webId',
-    array('controller' => 'articles', 'action' => 'viewGame'),
-    array('gameAlias' => '[^\:]+', 'slug' => '[^\:]+', 'webId' => '[^\:]+')
-);
 
 /**
  * Load all plugin routes. See the CakePlugin documentation on
