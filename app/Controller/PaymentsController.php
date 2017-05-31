bf0b8595295ef552df5a1057d0680b854c63840b
QuanVu <quanhongvu@gmail.com>
2017-06-01 00:32:38 +0700

    update payment

--- app/Controller/PaymentsController.php
+++ app/Controller/PaymentsController.php
@@ -7,7 +7,6 @@ class PaymentsController extends AppController {
 	public function beforeFilter()
 	{
 		parent::beforeFilter();
-		$this->Auth->allow();
         $this->layout = 'default_bootstrap';
 	}
 
