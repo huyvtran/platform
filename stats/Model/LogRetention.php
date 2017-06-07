2cf87b1029bc3d7bee50017d448c654632c8e77f
QuanVu <quanhongvu@gmail.com>
2017-06-07 10:25:28 +0700

    update

--- stats/Model/LogRetention.php
+++ stats/Model/LogRetention.php
@@ -4,7 +4,7 @@ App::uses('AppModel', 'Model');
 
 class LogRetention extends AppModel {
 
-	public $useTable = 'log_retention_by_day';
+    public $useTable = 'log_retention_by_day';
 	public $belongsTo = array('Game');
 
     public function retentionToChart($data, $games, $from = null, $to = null, $returnDate)
