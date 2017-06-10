b4d253177a671d4391cff6c1b6dfb6221130f2bc
QuanVu <quanhongvu@gmail.com>
2017-06-10 18:16:49 +0700

    update

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -40,7 +40,8 @@ class UsersController extends AppController {
 		$this->Auth->allow( 
 			'logout', 'api_register', 'api_login', 'api_change_password',
 			'api_login_takan', 'api_register_takan',
-			'api_login_ldr', 'api_register_ldr', 'api_change_password_ldr'
+			'api_login_ldr', 'api_register_ldr', 'api_change_password_ldr',
+			'api_register_v26', 'api_login_v26'
 		);
 	}
 
