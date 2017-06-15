371205bd758be7ea7a1cdc179891d596d589e347
QuanVu <quanhongvu@gmail.com>
2017-06-15 09:10:17 +0700

    update lỗi

--- app/Lib/Vippay.php
+++ app/Lib/Vippay.php
@@ -160,7 +160,7 @@ class Vippay {
                 )
             );
         } elseif (isset($resultVippay->code)
-            && in_array( $resultVippay->code, array(1000, 1007, 1009) )
+            && in_array( $resultVippay->code, array(1000, 1007, 1008, 1009) )
         ){
             # update trạng thái thẻ lỗi hoặc đã được sử dụng
             $result = array(
