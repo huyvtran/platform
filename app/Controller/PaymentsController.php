bb64fc5bf9280dd4c8145527d6643f31736d7805
Cris <cris@vntap.vn>
2018-04-04 09:31:44 +0700

    update

--- app/Controller/PaymentsController.php
+++ app/Controller/PaymentsController.php
@@ -89,15 +89,15 @@ class PaymentsController extends AppController {
             $order_id = microtime(true) * 10000;
 
             # sử dụng inpay
-            $keyRedis = 'error-payment-inpay-' . Payment::CHANEL_INPAY;
-            App::import('Lib', 'RedisCake');
-            $Redis = new RedisCake('action_count');
-            $Redis->key = $keyRedis;
-            $count = $Redis->get($keyRedis);
-            if ($count < 9) {
-                $chanel = Payment::CHANEL_INPAY;
-                $order_id = date('YmdHms') . rand(100, 999);
-            }
+//            $keyRedis = 'error-payment-inpay-' . Payment::CHANEL_INPAY;
+//            App::import('Lib', 'RedisCake');
+//            $Redis = new RedisCake('action_count');
+//            $Redis->key = $keyRedis;
+//            $count = $Redis->get($keyRedis);
+//            if ($count < 9) {
+//                $chanel = Payment::CHANEL_INPAY;
+//                $order_id = date('YmdHms') . rand(100, 999);
+//            }
 
             $data = $this->request->data;
             $data = array_merge($data, array(
