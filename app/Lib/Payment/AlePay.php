7c7bc338dba21de3350bc193e54ba405049d0bc9
Cris <cris@vntap.vn>
2018-05-09 16:43:32 +0700

    revert

--- app/Lib/Payment/AlePay.php
+++ app/Lib/Payment/AlePay.php
@@ -8,7 +8,7 @@ class AlePay {
     protected $appkey;
 
     protected $checkout_type = 1; // chỉ thanh toán thường
-    protected $currency = 'VND';
+    protected $currency = 'USD';
 
     protected $order_id;
 
