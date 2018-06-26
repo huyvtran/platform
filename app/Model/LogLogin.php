50782e47b4739fff4c394dffe43176c9c758be3b
Cris <cris@vntap.vn>
2018-06-26 16:39:06 +0700

    update

--- app/Model/LogLogin.php
+++ app/Model/LogLogin.php
@@ -6,23 +6,24 @@ App::uses('AppModel', 'Model');
  * Lưu record chứa thông tin thiết bị của users tại thời điểm login
  * có thể sử dụng để thống kê thông tin thiết bị sử dụng.
  */
-class LogLogin extends AppModel {
+class LogLogin extends AppModel
+{
 
-	public $validate = array(
-		'user_id' => array(
-			'rule'       => 'notEmpty',
-			'required'   => true,
-			'allowEmpty' => false
-		),
-		'game_id' => array(
-			'rule'       => 'notEmpty',
-			'required'   => true,
-			'allowEmpty' => false
-		)
-	);
+    public $validate = array(
+        'user_id' => array(
+            'rule' => 'notEmpty',
+            'required' => true,
+            'allowEmpty' => false
+        ),
+        'game_id' => array(
+            'rule' => 'notEmpty',
+            'required' => true,
+            'allowEmpty' => false
+        )
+    );
 
     function paginateCount($conditions = array(), $recursive = 0, $extra = array())
     {
-        return 10;
+        return 100;
     }
 }
