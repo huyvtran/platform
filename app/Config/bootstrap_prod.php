504e91119b9fae664132b3ca9d7c34a27f86f6da
Cris <cris@vntap.vn>
2018-11-20 11:17:54 +0700

    update

--- app/Config/bootstrap_prod.php
+++ app/Config/bootstrap_prod.php
@@ -209,9 +209,19 @@ Configure::write('Bot', array(
     'Telegram' => true
 ));
 
+//Configure::write('Paypal', array(
+//    'clientId'  => 'Ac56pz3p7V9ZjoOR4E6pdFX9gi8BjyBsfSwLZYhjMVUrA9yvM4zNNN6ilX70tOnVAFx68rk9G8J9NdMx', // abunguyen.com@gmail.com
+//    'secret'    => 'ECT3M4j6AgopanCNCVh-v-6CW70J1Rdenh3a7Lo70y-jOhxhoGdpsMyfiPeEpSE4ZAf28YtJzzkfQgL1',
+//    'ReturnUrl' => 'http://admin.muoriginfree.com:8880/OvsPayments/pay_paypal_response',
+//    'CancelUrl' => 'http://admin.muoriginfree.com:8880/OvsPayments/pay_error',
+//    'TokenUrl'  => 'https://api.paypal.com/v1/oauth2/token',
+//    'PaymentUrl'=> 'https://api.paypal.com/v1/payments/payment/',
+//    'mode'      => 'live',
+//));
+
 Configure::write('Paypal', array(
-    'clientId'  => 'Ac56pz3p7V9ZjoOR4E6pdFX9gi8BjyBsfSwLZYhjMVUrA9yvM4zNNN6ilX70tOnVAFx68rk9G8J9NdMx', // abunguyen.com@gmail.com
-    'secret'    => 'ECT3M4j6AgopanCNCVh-v-6CW70J1Rdenh3a7Lo70y-jOhxhoGdpsMyfiPeEpSE4ZAf28YtJzzkfQgL1',
+    'clientId'  => 'AVY70KpdjbpdjwPz7KzaDNVhqg8Pp4s4fQaSUnkLs0SILzo9SFSA5OGT8KTtTX4HODHywyR-OzeCg1Pn', // firestork@vntap.vn
+    'secret'    => 'EE3-3itBPi4LrkTugXvgWGaj-SSHI8f2N6INY4wOWikPP6y71SLLStSvDsZ0GFZvMp-GvCvJxkQe2-T3',
     'ReturnUrl' => 'http://admin.muoriginfree.com:8880/OvsPayments/pay_paypal_response',
     'CancelUrl' => 'http://admin.muoriginfree.com:8880/OvsPayments/pay_error',
     'TokenUrl'  => 'https://api.paypal.com/v1/oauth2/token',
